f08797c8fb55a1165482356ff942d54f0cf01fe0
celine.cogny@gmail.com <celine.cogny@gmail.com@b8f8b3be-d4df-c26d-983f-4e3e22d76dd1>
2011-03-26 11:50:53 +0000

    Complétion de la classe Projectile avec un constructeur arès l'avoir fait hériter de Block. C'était surtout un petit test :)

--- Angry-Imac/src/object/Projectile.java
+++ Angry-Imac/src/object/Projectile.java
@@ -1,5 +1,14 @@
 package object;
 
-public class Projectile {
+import org.jbox2d.common.Vec2;
 
+public class Projectile extends Block{
+
+	/**
+	 * Projectile's constructor
+	 * 
+	 */
+	Projectile(Shape s, int width, int height, Vec2 pos, float angle, Mat m) {
+		super(s, width, height, pos, angle, m);
+	}
 }
