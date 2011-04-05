3109c0bb901a8263aed6e12179af5cd0d8867e90
Timothee.roldao@gmail.com <Timothee.roldao@gmail.com@b8f8b3be-d4df-c26d-983f-4e3e22d76dd1>
2011-04-05 16:49:58 +0000

    Mise en place de la structure de la cible

---
+++ Angry-Imac/src/object/materials/Target.java
@@ -0,0 +1,34 @@
+package object.materials;
+
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.TexturePaint;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Target  extends Material{
+	public Target(){
+		density = 0.9f;
+		friction = 0.03f;
+		restitution = 0.01f;
+		weightFactor = 0;
+		breakable = true;
+		breakableForce = 50.0f;
+		color = new Color(126, 186, 190);
+		
+		Image img=null;
+        try {
+        	img=ImageIO.read(new File("textures/cible.jpg"));
+        }
+        catch(IOException e){
+        	System.out.println("ok");System.exit(0);
+        }
+        
+        ///g2.drawImage(img,0, 0, gg);
+        this.texture = new TexturePaint((BufferedImage) img, new Rectangle((int)(-img.getWidth(null)/2), (int)(-img.getHeight(null)/2),(int)(img.getWidth(null)), (int)(img.getHeight(null))));
+	}
+}
