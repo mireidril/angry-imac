b6c630ef0a2cd262f5fa60dc895707a0a76fc488
celine.cogny <celine.cogny@b8f8b3be-d4df-c26d-983f-4e3e22d76dd1>
2011-05-14 12:29:21 +0000

    Céline : Elastique plus joli & nouveau type de projectile mis en place (si le mammouth est comme celui du niveau 5, cliquez une deuxième fois quand il est lancé pour qu'il s'écrase au sol)

--- trunk/Angry-Imac/src/object/Projectile.java
+++ trunk/Angry-Imac/src/object/Projectile.java
@@ -3,25 +3,33 @@ package object;
 import org.jbox2d.common.Vec2;
 /**
  * Classe d'un Projectile 
- * @author BRUNELIERE Adrien, CHARBONNIER Fiona, COGNY Céline, KIELB Adrien et ROLDAO Timothée
+ * @author BRUNELIERE Adrien, CHARBONNIER Fiona, COGNY C≈Ωline, KIELB Adrien et ROLDAO Timoth≈Ωe
  * @version 1.0
  */
 public class Projectile extends Block{
 
 	public boolean active;
 	public boolean throwed;
+	public int type;
 	/**
 	 * Constructeur du projectile
+	 * @param t			Type de projectile
 	 * @param s			Shape du projectile
 	 * @param width		Largeur du projectile
 	 * @param height	Hauteur du projectile
 	 * @param pos		Position du projectile
 	 * @param angle		Angle d'inclinaison du projectile
-	 * @param m			Matière du projectile
+	 * @param m			Matiere du projectile
 	 */
-	public Projectile(Shape s, int width, int height, Vec2 pos, float angle, Mat m) {
+	public Projectile(int t, Shape s, int width, int height, Vec2 pos, float angle, Mat m) {
 		super(s, width, height, pos, angle, m);
 		active = false;
 		throwed = false;
+		if(t != -1) {
+			type = t;
+		}
+		else {
+			type = 0;
+		}
 	}
 }
